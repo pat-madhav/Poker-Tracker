1d4d0926e68e7549df939af0021b200d23232a5d
Pat Madhavaram <pattabhi.madhav@gmail.com>
2024-02-02 00:31:43 -0600

    Initial Commit

---
+++ Poker Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Poker Tracker
+//
+//  Created by Pattabhi Madhavaram on 2/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Poker TrackerUITests/Poker_TrackerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Poker_TrackerUITestsLaunchTests.swift
+//  Poker TrackerUITests
+//
+//  Created by Pattabhi Madhavaram on 2/2/24.
+//
+
+import XCTest
+
+final class Poker_TrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
